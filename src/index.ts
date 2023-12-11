9986bdba32e30fd2ba058694fd64a5eb717e9c7f
luzixu <lurongv@qq.com>
2023-12-11 18:10:46 +0800

    增加 splitTextToLines 方法

--- src/index.ts
+++ src/index.ts
@@ -1023,3 +1023,54 @@ export function isFullWidthChar(char: string) {
 
     return false
 }
+
+export interface SplitTextToLinesOptions {
+    /** 每一行的最大宽度，全角占据 2，半角占据 1 */
+    maxWidth?: number
+    /** 最大行数 */
+    maxLines?: number
+}
+
+export function splitTextToLines(text: string, options?: SplitTextToLinesOptions): string[] {
+    interface Line {
+        str: string
+        length: number
+    }
+    const { maxWidth = Infinity, maxLines = Infinity } = options ?? {}
+    const lines: Line[] = [{ str: "", length: 0 }]
+    let overflow = false
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i]
+        const charLength = isFullWidthChar(char) ? 2 : 1
+        const line = lines[lines.length - 1]
+        if (line.length + charLength <= maxWidth) {
+            line.str += char
+            line.length += charLength
+            continue
+        }
+        if (lines.length >= maxLines) {
+            overflow = true
+            break
+        }
+        lines.push({
+            str: char,
+            length: charLength
+        })
+    }
+    if (overflow) {
+        const last = lines[lines.length - 1]
+        let { str, length } = last
+        let final = 0
+        for (let i = 0; i < str.length; i++) {
+            const char = str[str.length - 1 - i]
+            const charLength = isFullWidthChar(char) ? 2 : 1
+            if (length - charLength + 3 <= maxWidth) {
+                final = i
+                break
+            }
+            length -= charLength
+        }
+        last.str = str.slice(0, -1 - final) + "..."
+    }
+    return lines.map(line => line.str)
+}
