38aba9d79decfa791e51b84a1c8222eb98104a01
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-05-20 15:26:57 +0800

    ✨feature: 去除无用的类型声明

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,3 @@
-declare module "is-equal" {
-    export default function isEqual(a: any, b: any): boolean
-}
-
 declare module "robust-segment-intersect" {
     export default function robustSegmentIntersect(a0: number[], a1: number[], b0: number[], b1: number[]): boolean
 }
