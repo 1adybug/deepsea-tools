218c3e89fdbef298392473b190af56172b294a16
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-05-23 14:58:20 +0800

    ✨feature: 为 getArray 再添加一种重载情况

--- src/index.ts
+++ src/index.ts
@@ -345,13 +345,20 @@ export function getPropertiesIsModified<T>(a: T, b: T): (...keyList: (keyof T)[]
     }
 }
 
-/** 获得一个函数循环出来的数组 */
+/**
+ * 获得一个函数循环出来的数组
+ * @param {number} length - 数组的长度
+ * 1. 不传第二个参数时，会返回一个 index 的数组，比如 getArray(3) => [0, 1, 2]
+ * 2. 传入一个函数时，会返回一个根据函数生成的数组，比如 getArray(3, index => index * 2) => [0, 2, 4]
+ * 3. 传入一个值时，会返回一个根据值生成的数组，比如 getArray(3, 2) => [2, 2, 2]
+ */
 export function getArray(length: number): number[]
 export function getArray<T>(length: number, fun: (index: number) => T): T[]
-export function getArray<T>(length: number, fun?: (index: number) => T): (T | number)[] {
+export function getArray<T>(length: number, item: T): (T extends Function ? never : T)[]
+export function getArray<T>(length: number, fun?: any): (T | number)[] {
     return Array(length)
         .fill(0)
-        .map((item, index) => (fun ? fun(index) : index))
+        .map((item, index) => (typeof fun === "function" ? fun(index) : fun ?? index))
 }
 
 /** 获取点到线的最短距离 */
